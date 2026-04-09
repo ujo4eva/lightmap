f008a9245637ff29c8073f61b1e6394663c04ab4
ujo4eva <git@polar7157.anonaddy.com>
2026-04-09 18:53:49 +0100

    You missed a spot lol @Gabes

--- server/run.py
+++ server/run.py
@@ -4,5 +4,5 @@ app = create_app()
 
 if __name__ == "__main__":
     print("🚀 Starting LightMap Server...")
-    print("Dashboard available at http://127.0.0.1:5000")
+    print("Dashboard available at http://127.0.0.1:5003")
     app.run(host="0.0.0.0", port=5003, debug=True, use_reloader=False)
